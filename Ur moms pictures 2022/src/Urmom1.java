1d23f2c58c7c25e63b5c0e5476ebc1316608c4f9
KantancheeKIRA <kantancheekhang@gmail.com>
2022-11-16 19:04:39 -0600

    I changed my life choices.

---
+++ Ur moms pictures 2022/src/Urmom1.java
@@ -0,0 +1,21 @@
+// UwU
+
+/*
+
+
+
+*/
+public class Urmom1{ // create class Urmom
+    // inside of these brakets defines tyhe classes scope
+
+    /*
+    Every jaja program needs a main method 
+    it is created like so below
+    */
+     public static void main(String[] args) { // create main method
+        // printing to console
+        System.out.println("Dad went for milk");
+        
+    }
+
+}
